25a285fe960c1850cdcb4aff29f63876aed1d466
timg-a <b2029tgegaleos@pshs.edu.ph>
2025-08-05 11:14:53 +0800

    First non-graded exercise

--- public/script.js
+++ public/script.js
@@ -1,1 +1,5 @@
-// no script tag needed here
+var age = 13;
+var maxAge = 122;
+var numPerDay = 3;
+
+document.write("You will need " + (numPerDay * 365) * (maxAge - age) + " sandwiches to last you until the ripe old age of " + maxAge + ".");
