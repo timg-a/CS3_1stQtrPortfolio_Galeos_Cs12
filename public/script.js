883070631497ae1a734af9dbd6b406c84ecc2f03
timg-a <b2029tgegaleos@pshs.edu.ph>
2025-08-05 11:33:34 +0800

    Third Commit

--- public/script.js
+++ public/script.js
@@ -1,5 +1,6 @@
 var age = 13;
 var maxAge = 122;
 var numPerDay = 3;
+var totalNeeded = (numPerDay * 365) * (maxAge - age);
 
-document.write("You will need " + (numPerDay * 365) * (maxAge - age) + " sandwiches to last you until the ripe old age of " + maxAge + ".");
+document.write("You will need " + totalNeeded + " sandwiches to last you until the ripe old age of " + maxAge + ".");
