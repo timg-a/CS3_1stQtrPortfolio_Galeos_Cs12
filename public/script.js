e043a5f2637761af371b69cbeb144594d1e59e23
timg-a <b2029tgegaleos@pshs.edu.ph>
2025-08-12 11:47:18 +0800

    script.js change

--- public/script.js
+++ public/script.js
@@ -3,4 +3,10 @@ var maxAge = window.prompt("Enter the age you would want to live to in years:", 
 var numPerDay = window.prompt("Enter how many snacks you eat per day:", "0");
 var totalNeeded = (maxAge - age) * numPerDay;
 
-document.write(`You need ${totalNeeded} snacks to last until the ripe old age of ${maxAge}`)
+var confirmation = window.confirm(`You entered: \n${age} for age\n${maxAge} for the age you want to live to\n${numPerDay} for the muber of snacks you eat per day\n\nIs your input correct?`);
+
+if (confirmation == true) {
+    document.write(`You need ${totalNeeded} snacks to last until the ripe old age of ${maxAge}`);
+} else {
+    window.alert("Run this code again.");
+}
