467b875f17ab3523359cd4d83e558972dba6f3f0
timg-a <b2029tgegaleos@pshs.edu.ph>
2025-08-12 11:17:41 +0800

    script.js change

---
+++ public/experiments/experiment1.js
@@ -0,0 +1,6 @@
+var color = "red";
+var sameColor = color === "red";
+var diffColor = color === "blue";
+
+document.write(`${sameColor} or ${diffColor}?` + "<br>");
+document.write("this is a " + typeof color + ".<br>");
